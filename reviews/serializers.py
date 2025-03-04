887eb8d8d18c6ee3bc31799411e3bcc696ae2b64
KudakwasheMurungweni <kudakwashemurungweni50@gmail.com>
2025-03-04 23:16:11 +0200

    Need to fix Reviews Api endpoint

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -5,3 +5,10 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+        read_only_fields = ['user']  # Prevent users from modifying this field
+
+    def validate_rating(self, value):
+        """Ensure rating is between 1-5"""
+        if not 1 <= value <= 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5")
+        return value
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
-from .models import Review
 from rest_framework.permissions import IsAuthenticated
+from .models import Review
 from .serializers import ReviewSerializer
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']  # Explicitly allow POST
+
+    def get_queryset(self):
+        """Return only the current user's reviews"""
+        return Review.objects.filter(user=self.request.user.profile)
+
+    def perform_create(self, serializer):
+        """Auto-assign the user's profile when creating a review"""
+        serializer.save(user=self.request.user.profile)
